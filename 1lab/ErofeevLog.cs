0dada65af6f6c43f202396bcf967b3dc7098cf66
Прог Антон <veefory@yandex.ru>
2022-04-08 15:37:21 +0300

    Third lab done

---
+++ 1lab/ErofeevLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using core;
+
+namespace _2lab
+{
+    internal class ErofeevLog : LogAbstract, ILogInterface
+    {
+        List<string> logList = new List<string>();
+        public ILogInterface log(string str)
+        {
+            logList.Add(str);
+            return this;
+        }
+
+        public ILogInterface write()
+        {
+            Console.WriteLine(logList.ToArray());
+            return this;
+        }
+
+
+        private static ErofeevLog instance;
+
+        private ErofeevLog() { }
+        public static ErofeevLog I()
+        {
+            if(instance == null)
+            {
+                instance = new ErofeevLog();
+            }
+            return instance;
+        }
+    }
+}
---
+++ 1lab/ILogInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    internal interface ILogInterface
+    {
+        ILogInterface log(string str);
+        ILogInterface write();
+
+    }
+}
